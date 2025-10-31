c9939acb08ad7be09cda3f0ff8807910f2f3e178
Prajwal Patil <73753614+Prajwal-glitch@users.noreply.github.com>
2025-10-31 13:39:19 +0000

    Homework Completed

---
+++ 05-deployment/predict.py
@@ -0,0 +1,17 @@
+import pickle
+from fastapi import FastAPI
+from typing import Dict, Any
+
+app = FastAPI(title="lead-score")
+
+with open("pipeline_v1.bin", "rb") as f_in:
+    pipeline = pickle.load(f_in)
+
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    proba = pipeline.predict_proba(customer)[0,1]
+    return float(proba)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
